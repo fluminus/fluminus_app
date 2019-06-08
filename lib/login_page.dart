21ed88bf34772715704cf97dbf165ae974742f8b
Tan Yuanhong <leotan.yh@gmail.com>
2019-06-08 17:23:18 +0800

    Login page prototype

---
+++ lib/login_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:fluminus/home_page.dart';
+
+/**
+ * This is a rip-off from this GitHub repo: 😜
+ * https://github.com/putraxor/flutter-login-ui
+ */
+
+class LoginPage extends StatefulWidget {
+  static String tag = 'login-page';
+  @override
+  _LoginPageState createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 70.0,
+        child: Image.asset('assets/logo.png'),
+      ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.text,
+      autofocus: false,
+      autocorrect: false,
+      decoration: InputDecoration(
+        hintText: 'NUSNET ID (e.g. e0261888)',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      autocorrect: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+
+    final loginButton = new Container(
+      width: 320.0,
+      height: 60.0,
+      alignment: FractionalOffset.center,
+      decoration: new BoxDecoration(
+        color: const Color.fromRGBO(247, 64, 106, 1.0),
+        borderRadius: new BorderRadius.all(const Radius.circular(30.0)),
+      ),
+      child: new Text(
+        "Sign In",
+        style: new TextStyle(
+          color: Colors.white,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w300,
+          letterSpacing: 0.3,
+        ),
+      ),
+    );
+
+    return Scaffold(
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+            SizedBox(height: 48.0),
+            email,
+            SizedBox(height: 8.0),
+            password,
+            SizedBox(height: 24.0),
+            loginButton,
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:dynamic_theme/dynamic_theme.dart';
-import 'package:fluminus/announcement_page.dart';
-import 'package:fluminus/task_page.dart';
-import 'package:fluminus/forum_page.dart';
-import 'package:fluminus/file_page.dart';
-import 'package:fluminus/profile_page.dart';
+import 'package:fluminus/home_page.dart';
+import 'package:fluminus/login_page.dart';
 import 'package:fluminus/widgets/theme.dart' as theme;
 
 /// Disabling the scroll glow.
@@ -19,14 +15,18 @@ void main() async {
   brightness =
       (prefs.getBool('isDark') ?? false) ? Brightness.dark : Brightness.light;
   await DotEnv().load('.env');
-  runApp(MyApp(
+  runApp(App(
     brightness: brightness,
   ));
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
   final Brightness brightness;
-  MyApp({this.brightness});
+  final routes = <String, WidgetBuilder>{
+    LoginPage.tag: (context) => LoginPage(),
+    HomePage.tag: (context) => HomePage(),
+  };
+  App({this.brightness});
   @override
   Widget build(BuildContext context) {
     return new DynamicTheme(
@@ -35,13 +35,14 @@ class MyApp extends StatelessWidget {
             brightness == Brightness.light ? theme.lightTheme : theme.darkTheme,
         themedWidgetBuilder: (context, theme) {
           return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              theme: theme,
-              builder: (context, child) {
-                return ScrollConfiguration(
-                    behavior: MyBehavior(), child: child);
-              },
-              home: MyHomePage());
+            debugShowCheckedModeBanner: false,
+            theme: theme,
+            builder: (context, child) {
+              return ScrollConfiguration(behavior: MyBehavior(), child: child);
+            },
+            home: LoginPage(),
+            routes: routes,
+          );
         });
   }
 }
@@ -51,69 +52,5 @@ class MyBehavior extends ScrollBehavior {
   Widget buildViewportChrome(
       BuildContext context, Widget child, AxisDirection axisDirection) {
     return child;
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _currentIndex = 0;
-  final _pages = [
-    AnnouncementPage(),
-    TaskPage(),
-    ForumPage(),
-    FilePage(),
-    ProfilePage()
-  ];
-
-  BottomNavigationBarItem _navBarItem(
-      IconData activeIcon, IconData defaultIcon, String title) {
-    return BottomNavigationBarItem(
-        icon: Icon(defaultIcon,
-            size: 30, color: Theme.of(context).unselectedWidgetColor),
-        activeIcon: Icon(
-          activeIcon,
-          size: 30,
-          color: Theme.of(context).brightness == Brightness.light
-              ? Theme.of(context).primaryColor
-              : Theme.of(context).accentColor,
-        ),
-        title: Text(
-          title,
-          style: Theme.of(context).textTheme.button,
-        ));
-  }
-
-  void _onTabTapped(int index) {
-    setState(() {
-      _currentIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          type: BottomNavigationBarType.fixed,
-          currentIndex: _currentIndex,
-          onTap: _onTabTapped,
-          items: <BottomNavigationBarItem>[
-            _navBarItem(MdiIcons.home, MdiIcons.homeOutline, 'Home'),
-            _navBarItem(
-                MdiIcons.calendarCheck, MdiIcons.calendarCheckOutline, 'Task'),
-            _navBarItem(MdiIcons.forum, MdiIcons.forumOutline, 'Forum'),
-            _navBarItem(MdiIcons.fileFind, MdiIcons.fileFindOutline, 'File'),
-            _navBarItem(MdiIcons.settings, MdiIcons.settingsOutline, 'Profile'),
-          ]),
-      body: _pages[_currentIndex],
-    );
   }
 }
