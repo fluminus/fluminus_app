b2590f2efe99b55988956eaac7f220c37b7a6605
Tan Yuanhong <leotan.yh@gmail.com>
2019-06-08 21:52:53 +0800

    Fixed auth storage bug In `data.dart`, stored `_auth` so that everything still works.

--- lib/data.dart
+++ lib/data.dart
@@ -2,11 +2,19 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:luminus_api/luminus_api.dart';
 
+Authentication _auth;
 
 Future<Authentication> authentication() async {
-  final storage = FlutterSecureStorage();
-  return Authentication(username: await storage.read(key: 'nusnet_id'), password: await storage.read(key: 'nusnet_password'));
+  if (_auth != null) {
+    return _auth;
+  } else {
+    final storage = FlutterSecureStorage();
+    return _auth = Authentication(
+        username: await storage.read(key: 'nusnet_id'),
+        password: await storage.read(key: 'nusnet_password'));
+  }
 }
+
 List<Module> modules;
 List<Announcement> announcements = new List();
 DateTime smsStartDate = DateTime.now();
