c84bce6cc00b43197603399686b341b17603e266
xiaoyu <zhang.gong.chang3@gmail.com>
2019-06-25 09:32:34 +0800

    Add welcome page.

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -3,9 +3,6 @@ import 'package:fluminus/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/animation.dart';
-import 'package:luminus_api/luminus_api.dart';
-import 'main.dart' as main;
-import 'data.dart' as data;
 import 'dart:async';
 import 'package:flutter/services.dart';
 import 'package:flutter/scheduler.dart' show timeDilation;
@@ -167,6 +164,7 @@ class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
                                         TextFormField(
                                           obscureText: true,
                                           decoration: InputDecoration(
+                                            labelText: 'PASSWORD',
                                             hintText: 'Password',
                                             icon: Icon(Icons.lock_outline),
                                           ),
---
+++ lib/welcome_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+
+
+class WelcomePage extends StatefulWidget {
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Swiper(
+      ),
+    );
+  }
+}
