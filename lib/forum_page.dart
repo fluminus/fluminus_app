cdf936af5343fdaca803d7fbee8f4663b3d0e824
Tan Yuanhong <leotan.yh@gmail.com>
2019-06-28 17:50:24 +0800

    Minor update 1. added switch for enabling push notifications in profile page 2. added embedded webview for forum page

--- lib/forum_page.dart
+++ lib/forum_page.dart
@@ -1,8 +1,10 @@
-import 'dart:async';
-import 'dart:io';
+// import 'dart:async';
+// import 'dart:io';
 
+import 'package:fluminus/widgets/placeholder.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 
 /// Currently this page is used solely for testing purposes.
 class ForumPage extends StatefulWidget {
@@ -11,25 +13,6 @@ class ForumPage extends StatefulWidget {
 }
 
 class _ForumPageState extends State<ForumPage> {
-  final FirebaseMessaging _firebaseMsg = FirebaseMessaging();
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _firebaseMsg.getToken().then((val) {
-      print(val);
-    });
-    _firebaseMsg.configure(onMessage: (Map<String, dynamic> msg) async {
-      print("onMessage: $msg");
-    });
-    if (Platform.isIOS) {
-      StreamSubscription iosSubscription;
-            iosSubscription = _firebaseMsg.onIosSettingsRegistered.listen((data) {
-                // save the token  OR subscribe to a topic here
-            });
-            _firebaseMsg.requestNotificationPermissions(IosNotificationSettings());
-        }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,6 +21,10 @@ class _ForumPageState extends State<ForumPage> {
           title: Text(
         'Forum Page',
       )),
+      body: WebView(
+        initialUrl: 'https://forum.tyhome.site',
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
     );
   }
 }
